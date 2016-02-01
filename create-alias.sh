79e0b718623fbf69b13e40c23573b6bb6bef204b
buidanhquy <quy.buidanhquy@gmail.com>
2016-02-01 15:04:07 +0700

    checkpoint

---
+++ create-alias.sh
@@ -0,0 +1,4 @@
+alias oiuc-config-view="/home/ubuntu/workspace/configtools/config-view /home/ubuntu/workspace/oiuc-v2/databases/oiuc.db ics_config"
+alias riuc-config-view="/home/ubuntu/workspace/configtools/config-view /home/ubuntu/workspace/riuc-v1/databases/riuc.db riuc_config"
+alias oiuc-config-change="/home/ubuntu/workspace/configtools/config-change /home/ubuntu/workspace/oiuc-v2/databases/oiuc.db ics_config"
+alias riuc-config-change="/home/ubuntu/workspace/configtools/config-change /home/ubuntu/workspace/riuc-v1/databases/riuc.db riuc_config"
